f594ff0720b43a5bf8b1368a74b890230f4a3a69
Moona <moonapark1126@gmail.com>
2025-09-08 17:23:21 +0900

    spring 추가

---
+++ HRBank/src/main/java/com/codeit/HRBank/HrBankApplication.java
@@ -0,0 +1,13 @@
+package com.codeit.HRBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrBankApplication.class, args);
+	}
+
+}
