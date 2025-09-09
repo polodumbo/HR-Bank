0e4e680d456e9deec59987ddd2925f8976e3b9de
SeoChanGyu9 <sdopre12@gmail.com>
2025-09-09 17:09:48 +0900

    fix: repository interface변환, Jpa extends

--- src/main/java/com/codeit/HRBank/repository/EmployeeRepository.java
+++ src/main/java/com/codeit/HRBank/repository/EmployeeRepository.java
@@ -1,4 +1,7 @@
 package com.codeit.HRBank.repository;
 
-public class EmployeeRepository {
+import com.codeit.HRBank.domain.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
 }
--- src/main/java/com/codeit/HRBank/repository/FileRepository.java
+++ src/main/java/com/codeit/HRBank/repository/FileRepository.java
@@ -1,4 +1,7 @@
 package com.codeit.HRBank.repository;
 
-public class FileRepository {
+import com.codeit.HRBank.domain.File;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FileRepository extends JpaRepository<File,Long> {
 }
