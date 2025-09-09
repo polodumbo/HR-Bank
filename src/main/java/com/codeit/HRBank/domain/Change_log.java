fb26ead3b4bca2a3cf9ced9680a98b4afcdc30b5
SeoChanGyu9 <sdopre12@gmail.com>
2025-09-09 13:42:58 +0900

    feat : domain change_log 추가

---
+++ src/main/java/com/codeit/HRBank/domain/Change_log.java
@@ -0,0 +1,49 @@
+package com.codeit.HRBank.domain;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.time.Instant;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+@Entity
+@Table(name="change_Logs")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Change_log {
+
+    @Id //pk
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    long id;
+    @Column(name = "type",  nullable = false, length = 50)
+    private String type;
+    @Column(name = "memo", nullable = false, length = 50)
+    private String memo;
+    @Column(name = "ip_address", nullable = false, length = 50)
+    private String ip_address;
+    @CreatedDate
+    @Column(name = "at", updatable = false, nullable = false)
+    private Instant at;
+
+    @JoinColumn(name = "employee_number")
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Employee employee;  //employee_number
+
+}
---
+++ src/main/java/com/codeit/HRBank/domain/Department.java
@@ -0,0 +1,39 @@
+package com.codeit.HRBank.domain;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name= "departments")
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String name;
+
+    @Column(nullable = false, columnDefinition  = "TEXT")
+    private String description;
+
+    @Column(name = "established_date", nullable = false)
+    private LocalDateTime establishedDate;
+}
