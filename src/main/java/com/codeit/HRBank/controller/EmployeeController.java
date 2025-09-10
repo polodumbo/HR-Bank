4d3fb13e2de8f87fe5ef338be7ef7ec8f876d421
DoHanChoi <ehgks904@naver.com>
2025-09-10 17:50:48 +0900

    chore: feat/employee-registration

--- src/main/java/com/codeit/HRBank/controller/EmployeeController.java
+++ src/main/java/com/codeit/HRBank/controller/EmployeeController.java
@@ -1,4 +1,35 @@
 package com.codeit.HRBank.controller;
 
+import com.codeit.HRBank.domain.Employee;
+import com.codeit.HRBank.dto.response.EmployeeResponse;
+import com.codeit.HRBank.dto.request.EmployeeRegistrationRequest;
+import com.codeit.HRBank.service.EmployeeService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/employees")
+@RequiredArgsConstructor
 public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<EmployeeResponse> registerEmployee(
+        @RequestPart("employee") @Valid EmployeeRegistrationRequest request,
+        @RequestPart(value = "profile", required = false) MultipartFile profileImage) {
+
+        Employee newEmployee = employeeService.registerNewEmployee(request, profileImage);
+        EmployeeResponse response = EmployeeResponse.from(newEmployee);
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
 }
