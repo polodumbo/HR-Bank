9e567d00a262e6cdf5b7f061a0e66b660c273124
SeoChanGyu9 <sdopre12@gmail.com>
2025-09-11 09:21:23 +0900

    fix: 메인어플리케이션 어노테이션추가, 스키마 백업테이블 엔드타임 NN 제거

--- src/main/java/com/codeit/HRBank/HrBankApplication.java
+++ src/main/java/com/codeit/HRBank/HrBankApplication.java
@@ -3,9 +3,11 @@ package com.codeit.HRBank;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableJpaAuditing
+@EnableJpaAuditing // JPA Auditing 활성화 (생성일시 자동 주입)
+@EnableScheduling // 스프링 스케줄러 활성화
 public class HrBankApplication {
 
 	public static void main(String[] args) {
