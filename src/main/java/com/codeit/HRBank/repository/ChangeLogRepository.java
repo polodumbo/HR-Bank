3ef0807aed92aa5748fd0511856bf32597064850
SeoChanGyu9 <sdopre12@gmail.com>
2025-09-12 14:11:15 +0900

    fix: 수정이력목록 설계 1차완료

--- src/main/java/com/codeit/HRBank/repository/ChangeLogRepository.java
+++ src/main/java/com/codeit/HRBank/repository/ChangeLogRepository.java
@@ -1,8 +1,15 @@
 package com.codeit.HRBank.repository;
 
+import com.codeit.HRBank.domain.ChangeLogType;
 import com.codeit.HRBank.domain.Change_log;
+import com.codeit.HRBank.domain.Department;
+import java.time.LocalDate;
 import java.util.Optional;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ChangeLogRepository extends JpaRepository<Change_log, Long> {
 
@@ -10,4 +17,25 @@ public interface ChangeLogRepository extends JpaRepository<Change_log, Long> {
 
   Optional<Change_log> findFirstByOrderByAtDesc();
 
+
+    @Query("""
+                SELECT c FROM Change_log c
+                WHERE (:idAfter IS NULL OR c.id > :idAfter)
+                AND c.employeeNumber LIKE '%' || COALESCE(:employeeNumber, "") || '%'
+                AND c.memo LIKE '%' || COALESCE(:memo, "") || '%'
+                AND c.ipAddress LIKE '%' || COALESCE(:ipAddress, "") || '%'
+                AND c.at >= COALESCE(:atFrom, c.at)
+                AND c.at <= COALESCE(:atTo, c.at)
+                AND (:type IS NULL OR c.type = :type)
+            """)
+    Slice<Change_log> findByCondition(
+            @Param("employeeNumber") String employeeNumber,
+            @Param("type") ChangeLogType type,
+            @Param("memo") String memo,
+            @Param("ipAddress") String ipAddress,
+            @Param("atFrom") LocalDate atFrom,
+            @Param("atTo") LocalDate atTo,
+            @Param("idAfter") Long idAfter,
+            Pageable pageable
+    );
 }
